83d4ea86b5a878572341f3f2c6dad21d4eee150c
Muhammad Saad <pubg53407@gmail.com>
2023-08-13 15:07:02 +0500

    Updating Title og Page and Using destructuring assignment in getDragAfterElement in js

--- script.js
+++ script.js
@@ -28,12 +28,14 @@ function getDragAfterElement(container, y) {
   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
   return draggableElements.reduce((closest, child) => {
-    const box = child.getBoundingClientRect()
-    const offset = y - box.top - box.height / 2
+    const { top, height } = child.getBoundingClientRect()
+    const offset = y - top - height / 2
+    console.log(offset)
     if (offset < 0 && offset > closest.offset) {
       return { offset: offset, element: child }
     } else {
-      return closest
+      return closest;
     }
   }, { offset: Number.NEGATIVE_INFINITY }).element
 }
+
